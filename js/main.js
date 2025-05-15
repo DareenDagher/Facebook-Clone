c2d29fb898023017a889e704a046553c097ade38
DareenDagher <dareendagher@gmail.com>
2025-05-15 05:53:42 +0300

    custom gender in login page

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
 //enable popover
 const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
-const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl));
+
+
+//gender custom
+if(window.location.pathname === '/'){ //only on index page
+    const femaleBtn = document.querySelector('#female');
+    const maleBtn = document.querySelector('#male');
+    const customBtn = document.querySelector('#custom');
+    const genderCustom = document.querySelector('#gender-custom');
+
+    femaleBtn.addEventListener('change', function() {
+        genderCustom.classList.add('d-none');
+    });
+    maleBtn.addEventListener('change', function() {
+        genderCustom.classList.add('d-none');
+    });
+    customBtn.addEventListener('change', function() {
+        genderCustom.classList.remove('d-none');
+    });
+}
